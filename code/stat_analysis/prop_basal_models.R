302b111edc625dfb8e3ee66c7a80687b0ebd9dd6
Alyssa Cirtwill <alyssa.cirtwill@gmail.com>
2021-07-21 17:44:01 +0300

    fit models

--- code/stat_analysis/prop_basal_models.R
+++ code/stat_analysis/prop_basal_models.R
@@ -10,22 +10,34 @@ library(MuMIn)
 
 # data is full data, including basals
 data$network_ID=paste(data$Size,data$Connectance,data$Network,sep='_')
-for(r in 1:nrow(data)){
-    netdat=data[which(data$network_ID==data$network_ID[r]),]
+subdata=data[which(data$Disturbance==0.1),]
+for(r in 1:nrow(subdata)){
+    netdat=subdata[which(subdata$network_ID==subdata$network_ID[r]),]
     nBasal=length(which(netdat$in_Degree==0))
-    n=data$Size[r]
-    data$prop_Basal[r]=nBasal/n
+    n=subdata$Size[r]
+    subdata$prop_Basal[r]=nBasal/n
 }
 
 # consumers is heterotrophs only
-
+for(r in 1:nrow(consumers)){
+    netID=consumers$network_ID[r]
+    netter=subdata[which(subdata$network_ID==netID),]
+    consumers$prop_Basal[r]=netter$prop_Basal[1]
+}
+save.image('basal_models.Rdata')
 
 # A: Does persistence vary with proportion of basal resources?
-SCper=with(consumers,lm(Persistence~scale(prop_Basal)*scale(Size)*scale(Connectance)*scale(Disturbance)))
+BCper=with(consumers,lm(Persistence~scale(prop_Basal)*scale(Size)*scale(Connectance)*scale(Disturbance)))
 
 
 # B: How do motif profiles vary with prop_Basal and C?
     motdata=read.table('../../data/3sp_motif_profiles.tsv',header=TRUE)
+    motdata$network_ID=paste0(motdata$Size,'_',motdata$Connectance,'_',motdata$Network)
+    for(r in 1:nrow(motdata)){
+        netdat=subdata[which(subdata$network_ID==motdata$network_ID[r]),]
+        motdata$prop_Basal[r]=netdat$prop_Basal[1]
+    }
+
     motdata$mot_total=rowSums(motdata[,4:7])
     motdata$prop_chain=motdata$m12/motdata$mot_total
     motdata$prop_apparent=motdata$m6/motdata$mot_total
@@ -47,15 +59,17 @@ SCper=with(consumers,lm(Persistence~scale(prop_Basal)*scale(Size)*scale(Connecta
         consumers$net_omni[r]=motdata$prop_omni[which(motdata$netID==net)]
     }
 
-    save.image('analysis.Rdata')
+    save.image('basal_models.Rdata')
 
     total_lm=with(motdata,lm(mot_total~prop_Basal*Connectance))
     summary(total_lm)
 
     # PERMANOVA and variability of motif profiles
-    propdist=vegdist(motdata[,10:13],method="bray")
+    propdist=vegdist(motdata[,11:14],method="bray")
+
     propperm=adonis(propdist~motdata$prop_Basal*motdata$Connectance,perm=9999)
-    motdata$interact=paste0(motdata$prop_Basal,motdata$Connectance,sep=':')
+
+    motdata$interact=paste0(motdata$prop_Basal,':',motdata$Connectance)
     propinterdisp=betadisper(propdist,motdata$interact,type="centroid")
     Spropdisp=betadisper(propdist,motdata$prop_Basal,type="centroid")
     Cpropdisp=betadisper(propdist,motdata$Connectance,type="centroid")
@@ -69,19 +83,19 @@ SCper=with(consumers,lm(Persistence~scale(prop_Basal)*scale(Size)*scale(Connecta
     colnames(interdata2)=c("Distance","BC")
     interdata2=as.data.frame(interdata2)    
 
-    interdata2$SC=as.character(interdata2$BC)
+    interdata2$BC=as.character(interdata2$BC)
     for(r in 1:nrow(interdata2)){
         levelcode=interdata2$"BC"[r]
-        interdata2$S[r]=strsplit(levelcode,':')[[1]][1]
+        interdata2$B[r]=strsplit(levelcode,':')[[1]][1]
         interdata2$C[r]=strsplit(levelcode,':')[[1]][2]
     }
     write.table(interdata2,file='proportion_variability_BC.tsv',sep='\t')
 
     # LMs
-    pchainlm=with(motdata,lm(prop_chain~prop_Basal*Connectance))
-    pomnilm=with(motdata,lm(prop_omni~prop_Basal*Connectance))
-    papplm=with(motdata,lm(prop_apparent~prop_Basal*Connectance))
-    pdirlm=with(motdata,lm(prop_direct~prop_Basal*Connectance))
+    Bchainlm=with(motdata,lm(prop_chain~prop_Basal*Connectance))
+    Bomnilm=with(motdata,lm(prop_omni~prop_Basal*Connectance))
+    Bapplm=with(motdata,lm(prop_apparent~prop_Basal*Connectance))
+    Bdirlm=with(motdata,lm(prop_direct~prop_Basal*Connectance))
 
 
 # C: How does motif participation vary with other properties?
